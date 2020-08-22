0d48cca333a97210b93bac691a7aef5a6e1c67ee
Morad En-Najmi <ennajmimorad@gmail.com>
2020-08-22 19:48:53 +0200

    ok

--- src/main/java/com/hibernate/HibernateApplication.java
+++ src/main/java/com/hibernate/HibernateApplication.java
@@ -12,7 +12,7 @@ import com.hibernate.entities.Artikel;
 import com.hibernate.repo.dao.AbteilungRepo;
 import com.hibernate.repo.dao.ArtikelRepo;
 
-/**************** vor erste !! My first change !!  and now the second change !! ok habe ich gesehen **********************/
+/****************ok get u !! vor erste !! My first change !!  and now the second change !! ok habe ich gesehen **********************/
 
 @SpringBootApplication
 public class HibernateApplication {
